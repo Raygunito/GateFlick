1963746aebeeea03d0b92a6f0ca935c7bd88292d
Raygunito <112569656+Raygunito@users.noreply.github.com>
2023-11-21 00:49:01 +0100

    sample query in function handle_database

--- src/server/Database.py
+++ src/server/Database.py
@@ -1,5 +1,10 @@
 import json
 import psycopg2
+import Logger
+
+logger = Logger.startLogger(__name__)
+
+
 def startConnection():
     with open("db_login.json", "r") as js_file:
         db_logging = json.load(js_file)
@@ -10,10 +15,16 @@ def startConnection():
 def handle_database(message: str, step: int, cursor):
     if (step == 0):
         # On exécutera les requetes en fonction du step
+        cursor.execute(f"SELECT * FROM ticket WHERE id_ticket = '{message}'", )
+        result = cursor.fetchone()
+        print(result)
+        # si jamais on a un soucis ex = pas de ligne trouvé alors => return False
+        return result
+    if (step == 1):
+        # On exécutera les requetes en fonction du step
         # cursor.execute("""SELECT * FROM ucs;""")
         # result = cursor.fetchall()
         # print(result)
         # si jamais on a un soucis ex = pas de ligne trouvé alors => return False
         return
     return
-
