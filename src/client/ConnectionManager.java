74da8940b005f20498ff08e1e6a366434a86bb54
Raygunito <112569656+Raygunito@users.noreply.github.com>
2023-11-24 21:02:38 +0100

    Client is now operational

--- src/client/ConnectionManager.java
+++ src/client/ConnectionManager.java
@@ -1,26 +1,80 @@
-import java.util.Scanner;
 import java.net.Socket;
+import java.util.Scanner;
+
+import org.apache.log4j.Logger;
+
+import log.LoggerUtility;
 
 public class ConnectionManager {
+
+    public static final String DEFAULT_PORT = "1234";
+    public static final String DEFAULT_ADDRESS = "localhost";
+    private static Logger logger = LoggerUtility.getLogger(ConnectionManager.class, "text");
+
     public static Socket login() {
+        boolean success = false;
         String host = "";
-        int port = -1;
+        String port = "";
         Socket socket = null;
-        Scanner input = new Scanner(System.in);
-        while (socket == null) {
-            System.out.println("IP address to connect to :");
-            host = input.nextLine();
-            System.out.println("Port to listen :");
-            port = input.nextInt();
-            input.nextLine();
+        while (!success) {
+            host = getValidHostInput();
+            port = getValidPortInput();
             try {
-                socket = new Socket(host, port);
+                socket = new Socket(host, Integer.parseInt(port));
+                success = true;
             } catch (Exception e) {
                 System.err.println("Connection failed");
             }
         }
-        System.out.println("Successfully connected to IP address : " + host + " on port : " + port);
+        System.out.println("Successfully connected to IP address: " + host + " on port: " + port);
+        logger.info("Successfully connected to IP address: " + host + " on port: " + port);
         return socket;
     }
-}
 
+    public static String getValidPortInput() {
+        Scanner input = new Scanner(System.in);
+        while (true) {
+            System.out.println("Enter the server port [" + DEFAULT_PORT + "]: ");
+            String portStr = input.nextLine();
+
+            if (portStr.isEmpty()) {
+                return DEFAULT_PORT;
+            }
+
+            try {
+                int port = Integer.parseInt(portStr);
+                if (port >= 1 && port <= 65535) {
+                    return Integer.toString(port);
+                } else {
+                    System.err.println("Port must be between 1 and 65535.");
+                }
+            } catch (NumberFormatException e) {
+                System.err.println("Invalid port number. Please enter a valid integer.");
+            } catch (Exception e) {
+                System.err.println("Unexpected error while getting port ");
+            }
+        }
+
+    }
+
+    public static String getValidHostInput() {
+        Scanner input = new Scanner(System.in);
+        while (true) {
+            System.out.println("Enter the server IP address or hostname [" + DEFAULT_ADDRESS + "]: ");
+            String host = input.nextLine();
+
+            if (host.isEmpty()) {
+                return DEFAULT_ADDRESS;
+            }
+            try {
+                java.net.InetAddress.getByName(host);
+                return host;
+            } catch (java.net.UnknownHostException e) {
+                System.err.println("Invalid input. Please enter a valid IP address or hostname.");
+            } catch (Exception e) {
+                System.err.println("Unexpected error while getting hostname ");
+            }
+        }
+
+    }
+}
--- src/client/SingleThreadClient.java
+++ src/client/SingleThreadClient.java
@@ -1,66 +1,141 @@
 import java.net.Socket;
-import java.io.BufferedReader;
+import java.net.SocketException;
 import java.io.IOException;
+import java.net.SocketTimeoutException;
+import java.util.Scanner;
+import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
-import java.util.Scanner;
-
 import org.apache.log4j.Logger;
-
 import log.LoggerUtility;
+import java.util.regex.Pattern;
 
 public class SingleThreadClient {
+    private static final int SOCKET_TIMEOUT = 30 * 1000; // 30 seconds
     private Socket socket;
     private static Logger logger = LoggerUtility.getLogger(SingleThreadClient.class, "text");
+    private int step = 0;
+
+    // Array of regex patterns
+    private static final Pattern COM_PATTERN = Pattern.compile("^Com\\d{9}$");
+    private static final Pattern PORTIQUE_PATTERN = Pattern.compile("^Portique\\d{4}$");
+    private static final Pattern START_UPDATE_PATTERN = Pattern.compile("START UPDATE");
+    private static final Pattern RECEIVED_PATTERN = Pattern.compile("RECEIVED");
+    public static final Pattern[] STEP_PATTERNS = { COM_PATTERN, PORTIQUE_PATTERN, START_UPDATE_PATTERN,
+            RECEIVED_PATTERN };
+    private static final Pattern VALID_PATTERN = Pattern.compile("^VALID\\s.*$");
+    private static final Pattern REJECT_PATTERN = Pattern.compile("^REJECT\\s.*$");
+    private static final String[] OUTPUT_CLIENT = { "QR Code non reconnu", "Portique inconnu", "QR Code déjà utilisé",
+            "Timeout, veuillez voir l'accueil" };
 
     public void start() {
-        socket = ConnectionManager.login();
+        try {
+            socket = ConnectionManager.login();
+            configureSocket();
 
-        try (
-            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            PrintWriter writer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), "UTF-8"), true);
-            Scanner inputMsg = new Scanner(System.in)
-        ) {
-            String msgSent = "";
-            String response;
-
-            System.out.print("Enter the first message: ");
-            msgSent = inputMsg.nextLine();
-            writer.println(msgSent);
-
-            while (!socket.isClosed()) {
-                if (reader.ready()) {
-                    response = reader.readLine();
-                    System.out.println("Server response: " + response);
-
-                    if (response.equalsIgnoreCase("exit")) {
-                        break;
-                    }
-
-                    System.out.print("Enter the next message: ");
-                    msgSent = inputMsg.nextLine();
-                    writer.println(msgSent);
-
-                    if (msgSent.equalsIgnoreCase("exit")) {
-                        break;
-                    }
-                }
-            }
+            communicateWithServer();
 
         } catch (IOException e) {
-            e.printStackTrace();
+            handleIOException(e);
         } finally {
             closeConnection();
         }
+    }
+
+    private void configureSocket() throws IOException {
+        try {
+            socket.setSoTimeout(SOCKET_TIMEOUT);
+        } catch (SocketException se) {
+            handleSocketException(se);
+        }
+    }
+
+    private void communicateWithServer() {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                PrintWriter writer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), "UTF-8"), true);
+                Scanner inputMsg = new Scanner(System.in)) {
+            String msgSent = "";
+            String response;
+
+            while (!socket.isClosed()) {
+                try {
+                    if (step < 2) {
+                        while (!STEP_PATTERNS[step].matcher(msgSent).matches()) {
+                            msgSent = inputMsg.nextLine();
+                            if (!STEP_PATTERNS[step].matcher(msgSent).matches()) {
+                                logger.warn("Code not recognized by protocol tracing : " + msgSent);
+                            }
+                        }
+                    } else {
+                        msgSent = STEP_PATTERNS[step].pattern();
+                    }
+                    sendMessage(msgSent, writer);
+                    if (socket.isClosed()) {
+                        break;
+                    }
+
+                    response = receiveMessage(reader);
+                    System.out.println(response);
+                    logger.debug("Server response: " + response);
+                    if (VALID_PATTERN.matcher(response).matches()) {
+                        step++;
+                    } else {
+                        if (REJECT_PATTERN.matcher(response).matches()) {
+                            System.out.println(OUTPUT_CLIENT[step] + "- Restarting to the beginning");
+                            step = 0;
+                        }
+                    }
+                    if (step == 4) {
+                        closeConnection();
+                    }
+                    if (msgSent.equalsIgnoreCase("exit") || socket.isClosed()) {
+                        break;
+                    }
+                } catch (SocketTimeoutException ste) {
+                    handleSocketTimeoutException(ste, socket);
+                    break;
+                } catch (IOException e) {
+                    handleIOException(e, socket);
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            logger.warn(e);
+        }
+    }
+
+    private void sendMessage(String message, PrintWriter writer) {
+        writer.println(message);
+        logger.debug("Client sent: " + message);
+    }
+
+    private String receiveMessage(BufferedReader reader) throws IOException {
+        return reader.readLine();
     }
 
     private void closeConnection() {
         try {
             socket.close();
         } catch (IOException e) {
-            e.printStackTrace();
+            handleIOException(e);
         }
+    }
+
+    private void handleIOException(IOException e) {
+        logger.error("IOException occurred: ", e);
+    }
+
+    private void handleIOException(IOException e, Socket socket) {
+        logger.error("IOException occurred: ", e);
+    }
+
+    private void handleSocketTimeoutException(SocketTimeoutException ste, Socket socket) {
+        logger.error("Socket operation timed out: " + socket.getInetAddress());
+    }
+
+    private void handleSocketException(SocketException se) {
+        logger.error("Socket exception: ", se);
     }
 
     public static void main(String[] args) {
